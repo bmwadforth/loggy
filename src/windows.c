ce48d8632ed26e8f0ee4edf5939cf44bd603dde3
Brannon Wadforth <whiskey@digitalnode.io>
2020-12-21 21:23:50 +1100

    Converting project to cpp and adding windows sdk logic

--- src/windows.c
+++
@@ -1,6 +0,0 @@
-#include "../include/keylogger.h"
-#include <stdio.h>
-
-void getKeyStrokes() {
-    printf("Windows key stroke logging to be implemented");
-}
---
+++ src/windows.cpp
@@ -0,0 +1,6 @@
+#include "../include/keylogger.h"
+#include <windows.h>
+
+void getKeyStrokes() {
+
+}
